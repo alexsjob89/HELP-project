105ce545a8d88f16f6045a20cd55f3a44a386309
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2023-01-15 21:30:54 +0000

    work on progress

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const navbar = document.querySelector(".navbar");
+// var images = ['/images/layout-3.png', '/images/layout-4.png', '/images/layout-3.png', '/images/layout-4.png'];
 
+// let i = 0;
+
+// function slideShow() {
+//     document.getElementById("my-swiper").src = images[i];
+//     if (i < images.lenght - 1) {
+//         i++;
+//     } else {
+//         i = 0;
+//     }
+//     setTimeout("slideShow()", 2000)
+// }
+// window.onload = slideShow;
---
+++ testing/test.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.slideImg', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+
+
+    // And if we need scrollbar
+
+});
